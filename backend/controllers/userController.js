05f77e3c104b414113d7227eed317f2d9b0afb94
Shaswat <shaswatrajput7@gmail.com>
2024-01-08 19:44:53 +0530

    added update profile API

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -157,3 +157,22 @@ exports.updatePassword = catchAsyncErrors(async(req,res,next)=>{
 
 })
 
+//Edit profile
+exports.updateProfile = catchAsyncErrors(async(req,res,next)=>{
+    
+    const newDataObj = {
+        name:req.body.name,
+        email:req.body.email
+    }
+    //we will add cloudiary later
+    const user = await User.findByIdAndUpdate(req.user.id,newDataObj,{
+        new:true,
+        runValidators:true,
+        useFindAndModify:false,
+    })
+    res.status(200).json({
+        success:true,
+    })
+
+})
+
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,15 @@
 const express = require("express");
-const { registerUser, loginUser, logoutUser, forgetPassword, resetPassword, getUserDetails, updatePassword } = require("../controllers/userController");
+const { registerUser,
+    loginUser,
+    logoutUser,
+    forgetPassword,
+    resetPassword,
+    getUserDetails,
+    updatePassword,
+    updateProfile } = require("../controllers/userController");
 const router = express.Router();
-const {isAuthenticatedUser,authorizeRoles} = require("../middleware/auth")
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth")
+
 
 router.route("/register").post(registerUser);
 
@@ -13,7 +21,9 @@ router.route("/password/reset/:token").put(resetPassword);
 
 router.route("/logout").get(logoutUser);
 
-router.route("/me").get(isAuthenticatedUser,getUserDetails)
+router.route("/me").get(isAuthenticatedUser, getUserDetails)
 
-router.route("/password/update").put(isAuthenticatedUser,updatePassword)
+router.route("/password/update").put(isAuthenticatedUser, updatePassword)
+
+router.route("/me/update").put(isAuthenticatedUser, updateProfile)
 module.exports = router;
